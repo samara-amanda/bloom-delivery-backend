52273eda79683692accd0cb7a33baa9ac76cd139
Sam <samara.amanda01@gmail.com>
2021-06-23 19:40:01 -0400

    include cookies, current user methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::API
-    def current_user
+    include ::ActionController::Cookies 
 
+    def current_user
+       User.find_by_id(session[:user_id])
     end
 
     def logged_in?
-
+        !!current_user
     end
 end
