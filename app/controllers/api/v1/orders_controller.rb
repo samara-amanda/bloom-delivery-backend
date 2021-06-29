ed857c3221e8bf8dcdb306a3248ae67527c57320
Sam <samara.amanda01@gmail.com>
2021-06-28 22:53:21 -0400

    display oall orders conditional

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::OrdersController < ApplicationController
     def index
-        orders = Order.all
-        render json: OrderSerializer.new(orders)
+        if logged_in?
+            orders = current_user.orders
+            render json: OrderSerializer.new(orders)
+        else
+            render json: {
+                error: "You must be logged in to see orders"
+            }
+        end
     end
 
     def show
