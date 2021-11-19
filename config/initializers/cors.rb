6ade9fb0308f47bb2c8d856e2bfd6268dd450a82
Sam <samara.amanda01@gmail.com>
2021-11-19 09:02:02 -0500

    cors issue fix attempt 2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3000', 'http://bloom-delivery-api.herokuapp.com']
+    origins ['http://localhost:3000', 'https://bloom-delivery-api.herokuapp.com']
 
     resource '*',
       headers: :any,
