85d8b7baa0277062e4e7b858b9859415ad34aaf8
Sam <samara.amanda01@gmail.com>
2022-02-25 09:47:16 -0500

    gemfile update, cors origin removed, fixed error in session controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -17,6 +17,7 @@ class Api::V1::SessionsController < ApplicationController
             render json: UserSerializer.new(current_user)
         else 
             render json: {error: "No one is logged in"}
+        end
     end
 
     def destroy
